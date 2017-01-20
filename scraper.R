25122e326815d763b580d03d3fd90d46e2a2235c
jogall <joedgallagher@gmail.com>
2017-01-20 14:17:25 +0000

    updated scraper.R

--- scraper.R
+++ scraper.R
@@ -1,19 +1,20 @@
-library(jsonlite)
+require(jsonlite)
+require(magrittr)
 
-# Static API for all FPL player data
-d <- fromJSON("https://fantasy.premierleague.com/drf/bootstrap-static")
+players_list <-
 
-#find total number of players
-n_players <- max(d$elements$id)
-
-#scrape data for each player
-players_list <- list()
-for(i in 1:n_players){
-	url <- paste0("https://fantasy.premierleague.com/drf/element-summary/", i)
-	l <- fromJSON(url)
-	players_list[[i]] <- l
-	Sys.sleep(sample(seq(1, 2, by=0.001), 1))
-}
-
-#TODO: flatten list
-# players_df <- do.call("rbind", players_list)
+	# get total number of players from static FPL API
+	1:max(fromJSON("https://fantasy.premierleague.com/drf/bootstrap-static")$elements$id) %>%
+	1:max(.) %>%
+	
+	#scrape data for each player for each gameweek
+	lapply(function(x) {
+		Sys.sleep(sample(seq(1, 2, by=0.001), 1))
+		url <- paste0("https://fantasy.premierleague.com/drf/element-summary/", x)
+		df <- fromJSON(url)$history
+		data.frame(player_id = x, df[,c(7, 54, 4:6, 8, 16:51)])
+	} ) %>%
+	
+	#flatten to df
+	rbind.fill()
+	
